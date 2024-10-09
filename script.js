0f48c4a22228f76b92d17d33b9afaf5607a5fe9e
Zeedd-0 <110570899+Zeedd-0@users.noreply.github.com>
2024-10-09 17:46:15 -0300

    Adjust

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 //     divideContainer()
 // });
 
+
 // function -------------------------
 function divideContainer(dim = 16) {
     for (i = 0; i < dim * dim; i++) {
@@ -12,8 +13,20 @@ function divideContainer(dim = 16) {
         square.style.height = 'calc(80vh / ' + dim + ')';
         CONTAINER.appendChild(square);
     };
+    SQUARES = document.querySelectorAll('.square');
+    addTrail();
 }
+    
+function addTrail() {
+    SQUARES.forEach((square) => {
+        square.addEventListener('mouseover', (e) => {
+            e.target.classList.add('trail');
+        });
+    });
+}
+// }
 
 // VARIABLES --------------------------
 // global
 const CONTAINER = document.querySelector('#container');
+let SQUARES = document.querySelectorAll('.square');
